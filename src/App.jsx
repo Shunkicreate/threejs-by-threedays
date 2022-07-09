8aa855f5d101c4a4995f4bbc67c1fd34818d9aae
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2022-07-10 00:34:54 +0900

    clean code and

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 // import logo from './logo.svg';
 import "./App.css";
 import ThreeMain from "./ThreeMain";
-import Clock from "./Clock";
 import React, { useEffect, useRef } from "react";
-import PlayAudio from "./PlayAudio";
 
 function App() {
   // let time = Clock
@@ -19,23 +17,6 @@ function App() {
   }
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <PlayAudio></PlayAudio>
-      <Clock></Clock>
-      <audio id="audio" controls loop src="./audio.mp3"></audio> */}
       <ThreeMain></ThreeMain>
     </div>
   );
--- src/ThreeMain.jsx
+++ src/ThreeMain.jsx
@@ -4,18 +4,16 @@ import React, { useRef, useEffect } from "react";
 import Camera from "./Camera";
 import Pointlightings from "./Pointlightings";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// import Pointlightings from "./Pointlightings"
 
 const ThreeMain = () => {
-{/* <script src="https://unpkg.com/three@0.137.4/examples/js/controls/OrbitControls.js"></script> */}
   useEffect(() => {
     const rnd = document.getElementById("threejsrenderer");
     rnd.appendChild(renderer.domElement);
   });
   const camera = Camera;
-  // const controls = new THREE.OrbitControls(camera, document.body);
-  // controls.enableDamping = true;
-  // controls.dampingFactor = 0.2;
+  const controls = new OrbitControls(camera, document.body);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.03;
 
   let rot = 0;
   let radius = 200;
@@ -27,19 +25,6 @@ const ThreeMain = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setAnimationLoop(animation);
 
-  // document.body.appendChild(renderer.domElement);
-
-  // const geometry = new THREE.TorusGeometry(100, 30, 160, 100);
-  // const material = new THREE.MeshStandardMaterial({
-  //   color: 0x6699ff,
-  //   roughness: 0.5,
-  // });
-
-  // const mesh = new THREE.Mesh(geometry, material);
-  // scene.add(mesh);
-
-  // const particlesMesh = Particles
-  // scene.add(particlesMesh)
   scene.fog = new THREE.Fog(0x000000, 50, 10000);
 
   // 平行光源
@@ -54,7 +39,6 @@ const ThreeMain = () => {
     scene.add(value);
   });
 
-  // scene.add(Pointlightings)
   function onResize() {
     // サイズを取得
     const width = window.innerWidth;
@@ -68,36 +52,17 @@ const ThreeMain = () => {
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
   }
-  // const sphere = new THREE.SphereGeometry( 0.5, 16, 8 );
-  // const light1 = new THREE.PointLight( 0xff0040, 2, 50 );
-  // light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
-  // scene.add( light1 );
 
-  // const light2 = new THREE.PointLight( 0x0040ff, 2, 50 );
-  // light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
-  // scene.add( light2 );
-
-  // const light3 = new THREE.PointLight( 0x80ff80, 2, 50 );
-  // light3.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
-  // scene.add( light3 );
-
-  // const light4 = new THREE.PointLight( 0xffaa00, 2, 50 );
-  // light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
-  // scene.add( light4 );
 
   var y = 0;
   window.addEventListener("wheel", function (e) {
     y = e.wheelDeltaY;
-    // console.log("横スクロール：" + window.scrollX);
-    // console.log("縦スクロール：" + window.scrollY);
   });
 
   //animathion process
   function animation(time) {
-    // controls.update();
+    controls.update();
     console.log(y);
-    // mesh.rotation.x = time / 1000;
-    // mesh.rotation.y = time / 500;
     console.log(camera.position.x, camera.position.y, camera.position.z);
 
     rot += 0.25; // 毎フレーム角度を0.5度ずつ足していく
@@ -106,8 +71,6 @@ const ThreeMain = () => {
     // 角度に応じてカメラの位置を設定
     radius -= y / 5;
     y = 0;
-    camera.position.x = radius * Math.sin(radian);
-    camera.position.z = radius * Math.cos(radian);
     // 原点方向を見つめる
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     renderer.render(scene, camera);
